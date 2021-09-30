bc325246fdcefe86116668dd65ecba707cc9eef3
Chatman R. Jr <crjr.code@protonmail.com>
2021-09-29 21:48:19 -0400

    Generate fish config.

---
+++ env/fish/config.fish
@@ -0,0 +1,8 @@
+starship init fish | source
+
+# Setup Guix user environment
+set GUIX_PROFILE "$HOME/.guix-profile"
+bass source $GUIX_PROFILE/etc/profile
+
+# Setup Nix user environment
+bass source /run/current-system/profile/etc/profile.d/nix.sh
