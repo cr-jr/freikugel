2063fa05e2413bbbf9474e30db7661ca2f082041
Chatman R. Jr <crjr.code@protonmail.com>
2021-09-30 01:59:18 -0400

    Scaffold a basic prompt configuration.

--- env/fish/config.fish
+++ env/fish/config.fish
@@ -1,8 +1,9 @@
-starship init fish | source
-
 # Setup Guix user environment
 set GUIX_PROFILE "$HOME/.guix-profile"
 bass source $GUIX_PROFILE/etc/profile
 
 # Setup Nix user environment
 bass source /run/current-system/profile/etc/profile.d/nix.sh
+
+# Launch the starship
+starship init fish | source
