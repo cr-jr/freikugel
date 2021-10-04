e83e1bf5d9d5fa9238b30e5478c2f6cd37c93b27
Chatman R. Jr <crjr.code@protonmail.com>
2021-10-04 11:30:59 -0400

    Rename Apps.org to Addons.org and add package manifest

---
+++ addons/.guix-manifests/guix-addons-manifest.scm
@@ -0,0 +1,13 @@
+;; This manifest contains Guix packages for my daily apps.
+
+(specifications->manifest
+  '("qutebrowser"
+    "mpv"
+    "virt-manager"
+    "pcmanfm"
+    "syncthing-gtk"
+    "picard"
+    "krita"
+    "inkscape"
+    "scribus"
+    "sonata"))
